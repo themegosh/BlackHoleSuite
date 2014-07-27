34cae3ea016ec6ce71e8922ea2b9a2144d540388
themegosh <mathewdf@gmail.com>
2014-07-27 07:58:30 -0400

    Try and fix some administrator related crashes...

--- BlackHoleClient/Program.cs
+++ BlackHoleClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using BlackHoleLib;
+using System.Security.Principal;
 
 namespace BlackHoleClient
 {
@@ -17,24 +18,49 @@ namespace BlackHoleClient
             Application.SetCompatibleTextRenderingDefault(false);
             Application.EnableVisualStyles();
 
-            BlackHoleSuite.LoadConfig();
-
-            //if debugging, allow vs exception handling
-            if (System.Diagnostics.Debugger.IsAttached)
-                Application.Run(new frmModern());
+            if (!IsUserAdministrator())
+                MessageBox.Show("You need to run this program as Administrator to continue!", "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             else
             {
-                try //not debug mode, show error window instead
-                {
-                    Application.Run(new frmModern());
-                }
-                catch (Exception ex)
-                {
-                    (new frmError(ex)).ShowDialog(); //this type of error message is temporary
-                }
-            }
+                BlackHoleSuite.LoadConfig();
 
-            BlackHoleSuite.SaveConfig();
+                //if debugging, allow vs exception handling
+                if (System.Diagnostics.Debugger.IsAttached)
+                    Application.Run(new frmModern());
+                else
+                {
+                    try //not debug mode, show error window instead
+                    {
+                        Application.Run(new frmModern());
+                    }
+                    catch (Exception ex)
+                    {
+                        (new frmError(ex)).ShowDialog(); //this type of error message is temporary
+                    }
+                }
+
+                BlackHoleSuite.SaveConfig();
+            }
+        }
+
+        public static bool IsUserAdministrator()
+        {
+            bool isAdmin;
+            try
+            {
+                WindowsIdentity user = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(user);
+                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                isAdmin = false;
+            }
+            catch (Exception ex)
+            {
+                isAdmin = false;
+            }
+            return isAdmin;
         }
     }
 }
